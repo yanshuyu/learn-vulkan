6a46ad22183272abf8d5e0c74fdad6e5b685f299
yanshuyu <yanshuyu@aoshugame.com>
2023-12-26 22:01:20 +0800

    pipeline class (fix function stages)

---
+++ GraphicPipeline.cpp
@@ -0,0 +1,334 @@
+#include"GraphicPipeline.h"
+#include<algorithm>
+#include<iostream>
+
+GraphicPipeline::GraphicPipeline()
+{
+    m_RSCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    m_DSCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+    ResetDefaultStates();
+    m_Pipeline = VK_NULL_HANDLE;
+    m_Device = VK_NULL_HANDLE;
+}
+
+GraphicPipeline::~GraphicPipeline()
+{
+    Release();
+}
+
+
+void GraphicPipeline::VISetBinding(uint32_t location, uint32_t stride, VkVertexInputRate rate)
+{
+    auto pos = std::find_if(m_VIBindingDesc.begin(), m_VIBindingDesc.end(), [=](const VkVertexInputBindingDescription &bindingDesc)
+                            { return bindingDesc.binding == location; });
+                            
+    if (pos == m_VIBindingDesc.end())
+    {
+        m_VIBindingDesc.emplace_back();
+        pos = m_VIBindingDesc.end();
+        pos--;
+    }
+
+    pos->binding = location;
+    pos->stride = stride;
+    pos->inputRate = rate;
+}
+
+
+void GraphicPipeline::VISetAttribute(uint32_t location, uint32_t binding, VkFormat fmt, uint32_t offset)
+{
+    auto pos = std::find_if(m_VIAttrsDesc.begin(), m_VIAttrsDesc.end(), [=](const VkVertexInputAttributeDescription &attrDesc)
+                            { return attrDesc.location == location; });
+    if (pos == m_VIAttrsDesc.end())
+    {
+        m_VIAttrsDesc.emplace_back();
+        pos = m_VIAttrsDesc.end();
+        pos--;
+    }
+
+    pos->location = location;
+    pos->binding = binding;
+    pos->format = fmt;
+    pos->offset = offset;
+}
+
+
+void GraphicPipeline::IASetTopology(VkPrimitiveTopology pt, bool ptRestarted)
+{
+    m_IATopology = pt;
+    m_IAPrimitiveRestart = ptRestarted;
+}
+
+
+void GraphicPipeline::VSSetViewportScissorRect(VkRect2D viewportRect, VkRect2D scissorRect)
+{
+    m_Viewport.x = static_cast<float>(viewportRect.offset.x);
+    m_Viewport.y = static_cast<float>(viewportRect.offset.y);
+    m_Viewport.width = static_cast<float>(viewportRect.extent.width);
+    m_Viewport.height = static_cast<float>(viewportRect.extent.height);
+    m_Scissor = scissorRect;
+}
+
+
+ void GraphicPipeline::RSSetDepthBias(float constFactor, float slotFactor, float clamp)
+ {
+    m_RSCreateInfo.depthBiasConstantFactor = constFactor;
+    m_RSCreateInfo.depthBiasSlopeFactor = slotFactor;;
+    m_RSCreateInfo.depthBiasClamp = clamp;
+ }
+
+
+ void GraphicPipeline::ResetDefaultStates()
+ {
+    m_DynamicStates.clear();
+
+    m_VIBindingDesc.clear();
+    m_VIAttrsDesc.clear();
+
+    m_IATopology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    m_IAPrimitiveRestart = false;
+
+    m_Viewport.x = m_Viewport.y = 0;
+    m_Viewport.width = m_Viewport.height = 00;
+    m_Viewport.minDepth = 0;
+    m_Viewport.maxDepth = 1;
+    m_Scissor = {};
+
+    m_RSCreateInfo = GetDefaultRasterizationState();
+    
+    m_DSCreateInfo = GetDefaultDepthStencilState();
+
+    m_FBAttchmentStates.clear();
+
+    m_MsSampleCount = VK_SAMPLE_COUNT_1_BIT;
+    m_AlphaToCoverageEnable = false;
+
+ }
+
+
+
+void GraphicPipeline::FBEnableBlend(int attachmentIdx)
+{
+    if (attachmentIdx >= m_FBAttchmentStates.size())
+        m_FBAttchmentStates.resize(attachmentIdx + 1, GetDefaultBlendState());
+    
+    m_FBAttchmentStates[attachmentIdx].blendEnable = VK_TRUE;
+}
+
+void GraphicPipeline::FBDisableBlend(int attachmentIdx)
+{
+    if (attachmentIdx >= m_FBAttchmentStates.size())
+        return;
+
+     m_FBAttchmentStates[attachmentIdx].blendEnable = VK_FALSE;
+}
+
+bool GraphicPipeline::FBGetBlendEnabled(int attachmentIdx) const
+{
+    if (attachmentIdx >= m_FBAttchmentStates.size())
+        return false;
+    
+    return m_FBAttchmentStates[attachmentIdx].blendEnable;
+}
+
+
+void GraphicPipeline::FBSetColorBlendOp(int attachmentIdx, VkBlendFactor srcFactor, VkBlendFactor dstFactor, VkBlendOp op,  VkColorComponentFlags colorWriteMask)
+{
+    if (attachmentIdx >= m_FBAttchmentStates.size())
+        m_FBAttchmentStates.resize(attachmentIdx+1, GetDefaultBlendState());
+
+    m_FBAttchmentStates[attachmentIdx].blendEnable = VK_TRUE;
+    m_FBAttchmentStates[attachmentIdx].srcColorBlendFactor = srcFactor;
+    m_FBAttchmentStates[attachmentIdx].dstColorBlendFactor = dstFactor;
+    m_FBAttchmentStates[attachmentIdx].colorBlendOp = op;
+    m_FBAttchmentStates[attachmentIdx].colorWriteMask = colorWriteMask;
+}
+
+bool GraphicPipeline::FBGetColorBlendOp(int attachmentIdx, VkBlendFactor& srcFactor, VkBlendFactor& dstFactor, VkBlendOp& op, VkColorComponentFlags& colorWriteMask) const
+{
+    if (attachmentIdx >= m_FBAttchmentStates.size())
+        return false;
+
+    srcFactor = m_FBAttchmentStates[attachmentIdx].srcColorBlendFactor;
+    dstFactor = m_FBAttchmentStates[attachmentIdx].dstColorBlendFactor;
+    op = m_FBAttchmentStates[attachmentIdx].colorBlendOp;
+    colorWriteMask = m_FBAttchmentStates[attachmentIdx].colorWriteMask;
+
+    return true;
+}
+
+void GraphicPipeline::FBSetAlphaBlendOp(int attachmentIdx, VkBlendFactor srcFactor, VkBlendFactor dstFactor, VkBlendOp op)
+{
+    if (attachmentIdx >= m_FBAttchmentStates.size())
+        m_FBAttchmentStates.resize(attachmentIdx+1, GetDefaultBlendState());
+
+    m_FBAttchmentStates[attachmentIdx].srcAlphaBlendFactor = srcFactor;
+    m_FBAttchmentStates[attachmentIdx].dstAlphaBlendFactor = dstFactor;
+    m_FBAttchmentStates[attachmentIdx].alphaBlendOp = op;
+
+}
+
+bool GraphicPipeline::FBGetAlphaBlendOp(int attachmentIdx, VkBlendFactor& srcFactor, VkBlendFactor& dstFactor, VkBlendOp& op) const
+{
+    if (attachmentIdx >= m_FBAttchmentStates.size())
+        return false;;
+    
+    srcFactor = m_FBAttchmentStates[attachmentIdx].srcAlphaBlendFactor;
+    dstFactor = m_FBAttchmentStates[attachmentIdx].dstAlphaBlendFactor;
+    op = m_FBAttchmentStates[attachmentIdx].alphaBlendOp;;
+    
+    return true;
+}
+
+
+void GraphicPipeline::SetDynamicStateHint(VkDynamicState state)
+{
+    if (std::find(m_DynamicStates.begin(), m_DynamicStates.end(), state) == m_DynamicStates.end())
+        m_DynamicStates.push_back(state);
+}
+
+void GraphicPipeline::UnsetDynamicStateHint(VkDynamicState state)
+{
+    auto pos = std::find(m_DynamicStates.begin(), m_DynamicStates.end(), state);
+    if (pos == m_DynamicStates.end())
+        return;;
+    
+    m_DynamicStates.erase(pos);
+}
+
+bool GraphicPipeline::IsDynamicState(VkDynamicState state) const
+{
+    return std::find(m_DynamicStates.begin(), m_DynamicStates.end(), state) != m_DynamicStates.end();
+}
+
+
+void GraphicPipeline::MSSetSampleCount(int sampleCnt)
+{
+    switch (sampleCnt)
+    {
+    case 1:
+        m_MsSampleCount = VK_SAMPLE_COUNT_1_BIT;
+        break;
+    case 4:
+        m_MsSampleCount = VK_SAMPLE_COUNT_4_BIT;
+    case 16:
+        m_MsSampleCount = VK_SAMPLE_COUNT_16_BIT;
+    default:
+        break;
+    }
+}
+
+int GraphicPipeline::MSGetSampleCount() const
+{
+    switch (m_MsSampleCount)
+    {
+    case VK_SAMPLE_COUNT_4_BIT:
+        return 4;
+    case VK_SAMPLE_COUNT_16_BIT:
+        return 16;
+    
+    default:
+        return 1;
+    }
+}
+
+
+
+bool GraphicPipeline::Create(VkDevice device, bool forceCreate)
+{
+    if (device == VK_NULL_HANDLE)
+        return false;
+
+    if (forceCreate && IsCreate())
+        Release();
+    
+    VkPipelineVertexInputStateCreateInfo viInfo{};
+    viInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    viInfo.flags = 0;
+    viInfo.pNext = 0;
+    viInfo.vertexBindingDescriptionCount = m_VIBindingDesc.size();
+    viInfo.pVertexBindingDescriptions = m_VIBindingDesc.data();
+    viInfo.vertexAttributeDescriptionCount = m_VIAttrsDesc.size();
+    viInfo.pVertexAttributeDescriptions = m_VIAttrsDesc.data();
+
+    VkPipelineInputAssemblyStateCreateInfo iaInfo{};
+    iaInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    iaInfo.flags = 0;
+    iaInfo.pNext = nullptr;;
+    iaInfo.topology = m_IATopology;
+    iaInfo.primitiveRestartEnable = m_IAPrimitiveRestart;
+
+    VkPipelineViewportStateCreateInfo viewportScissorInfo{};
+    viewportScissorInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewportScissorInfo.flags = 0;
+    viewportScissorInfo.pNext = nullptr;
+    viewportScissorInfo.viewportCount = 1;
+    viewportScissorInfo.pViewports = &m_Viewport;
+    viewportScissorInfo.scissorCount = 1;
+    viewportScissorInfo.pScissors = &m_Scissor;
+
+    VkPipelineColorBlendStateCreateInfo fbInfo{};
+    fbInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    fbInfo.flags = 0;
+    fbInfo.pNext = nullptr;
+    fbInfo.logicOpEnable = false;
+    fbInfo.attachmentCount = m_FBAttchmentStates.size();
+    fbInfo.pAttachments = m_FBAttchmentStates.data();
+
+    VkPipelineMultisampleStateCreateInfo msInfo{};
+    msInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    msInfo.flags = 0;
+    msInfo.pNext = nullptr;
+    msInfo.alphaToCoverageEnable = m_AlphaToCoverageEnable;
+    msInfo.alphaToOneEnable = false;
+    msInfo.pSampleMask = nullptr;
+    msInfo.sampleShadingEnable = false;
+    msInfo.minSampleShading = 0;
+    msInfo.rasterizationSamples = m_MsSampleCount;
+
+    VkPipelineDynamicStateCreateInfo dyStateInfo{};
+    dyStateInfo.flags = 0;
+    dyStateInfo.pNext = nullptr;
+    dyStateInfo.dynamicStateCount = m_DynamicStates.size();
+    dyStateInfo.pDynamicStates = m_DynamicStates.data();
+
+    VkGraphicsPipelineCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;;
+    createInfo.pDynamicState = &dyStateInfo;
+    createInfo.pVertexInputState = &viInfo;;
+    createInfo.pInputAssemblyState = &iaInfo;
+    createInfo.pViewportState = &viewportScissorInfo;
+    createInfo.pRasterizationState = &m_RSCreateInfo;
+    createInfo.pDepthStencilState = &m_DSCreateInfo;
+    createInfo.pColorBlendState = &fbInfo;
+    createInfo.pMultisampleState = &msInfo;
+
+
+    VkPipeline createdPipeline = VK_NULL_HANDLE;
+    VkResult result = vkCreateGraphicsPipelines(device, nullptr, 1, &createInfo, nullptr, &createdPipeline);
+    if (result != VK_SUCCESS)
+    {
+        std::cout << "-->Create Graphics Pipeline Failed, Error: " << result << std::endl;
+    }
+
+    
+    if (result == VK_SUCCESS)
+    {
+        m_Device = device;
+        m_Pipeline = createdPipeline;
+    }
+
+    return IsCreate();
+}
+
+
+void GraphicPipeline::Release()
+{
+    if (IsCreate())
+    {
+        vkDestroyPipeline(m_Device, m_Pipeline, nullptr);
+        ResetDefaultStates();
+        m_Device = VK_NULL_HANDLE;
+        m_Pipeline = VK_NULL_HANDLE;
+    }
+}
---
+++ GraphicPipeline.h
@@ -0,0 +1,183 @@
+
+#pragma once
+#include<vulkan\vulkan.h>
+#include<vector>
+#include<numeric>
+
+using std::vector;
+
+class GraphicPipeline 
+{
+
+private:
+    // vertex input
+    vector<VkVertexInputBindingDescription> m_VIBindingDesc{};
+    vector<VkVertexInputAttributeDescription> m_VIAttrsDesc{};
+
+    // input assembly
+    VkPrimitiveTopology m_IATopology;
+    bool m_IAPrimitiveRestart;;
+
+    // viewport & scissor
+    VkViewport m_Viewport{};
+    VkRect2D m_Scissor{};
+
+    // rasterizer
+    VkPipelineRasterizationStateCreateInfo m_RSCreateInfo{};
+
+    // depth & stencil test
+    VkPipelineDepthStencilStateCreateInfo m_DSCreateInfo {};
+    
+    // framebuffer blending
+    vector<VkPipelineColorBlendAttachmentState> m_FBAttchmentStates {};
+
+    // dynamic state
+    vector<VkDynamicState> m_DynamicStates;
+
+    // multisample state
+    bool m_AlphaToCoverageEnable;
+    VkSampleCountFlagBits m_MsSampleCount;
+
+    // pipeline
+    VkPipeline m_Pipeline;
+    VkDevice m_Device;
+
+public:
+    GraphicPipeline();
+    ~GraphicPipeline();
+
+    //vertex input
+    void VISetBinding(uint32_t location, uint32_t stride, VkVertexInputRate rate = VK_VERTEX_INPUT_RATE_VERTEX);
+    VkVertexInputBindingDescription VIGetBinding(int idx) const { return m_VIBindingDesc[idx]; }
+    int VIGetBindingCount() const { return m_VIBindingDesc.size(); }
+
+    void VISetAttribute(uint32_t location, uint32_t binding, VkFormat fmt, uint32_t offset);
+    VkVertexInputAttributeDescription VIGetAttribute(int idx) const { return m_VIAttrsDesc[idx]; }
+    int VIGetAttributesCount() const { return m_VIAttrsDesc.size(); }
+
+    // input assembly
+    void IASetTopology(VkPrimitiveTopology pt, bool ptRestarted = false);
+    VkPrimitiveTopology IAGetTopology() const { return m_IATopology; }
+
+    // viewport & scissor
+    void VSSetViewportScissorRect(VkRect2D viewportRect, VkRect2D scissorRect);
+    VkRect2D VSGetScissorRect() const { return m_Scissor; }
+    VkRect2D VSGetViewportRect() const 
+    { 
+        VkRect2D rect;
+        rect.offset.x = m_Viewport.x;
+        rect.offset.y = m_Viewport.y;
+        rect.extent.width = m_Viewport.width;
+        rect.extent.height = m_Viewport.height;
+        return  rect;
+    }
+
+    // rasterization
+    void RSSetCullFace(VkCullModeFlags cullMode) { m_RSCreateInfo.cullMode = cullMode; }
+    void RSSetFillMode(VkPolygonMode fillMode) { m_RSCreateInfo.polygonMode = fillMode; }
+    void RSSetFrontFaceOrder(VkFrontFace faceOrder) { m_RSCreateInfo.frontFace = faceOrder; }
+    void RSEnableDepthBias() { m_RSCreateInfo.depthBiasEnable = VK_TRUE; }
+    void RSDisableDepthBias() { m_RSCreateInfo.depthBiasEnable = VK_FALSE; }
+    void RSSetDepthBias(float constFactor, float slotFactor, float clamp = 0);
+    VkCullModeFlags RSGetCullMode() const { return m_RSCreateInfo.cullMode; }
+    VkPolygonMode RSGetFillMode() const { return m_RSCreateInfo.polygonMode; }
+    VkFrontFace RSGetFrontFace()  const { return m_RSCreateInfo.frontFace; }
+    bool RSGetDepthBiasEnabled() const { return m_RSCreateInfo.depthBiasEnable; }
+
+    // depth & stencil test
+    void DSEnableZTest() { m_DSCreateInfo.depthTestEnable = VK_TRUE; }
+    void DSDisableZTest() { m_DSCreateInfo.depthTestEnable = VK_FALSE; }
+    bool DSGetZTestEnabled() const { return m_DSCreateInfo.depthTestEnable; }
+    void DSEnableZWrite() { m_DSCreateInfo.depthWriteEnable = VK_TRUE; }
+    void DSDisableZWrite() { m_DSCreateInfo.depthWriteEnable = VK_FALSE;}
+    bool DSGetZWriteEnabled() const { return m_DSCreateInfo.depthWriteEnable; }
+    void DSSetZCmpOp(VkCompareOp zCmpOp) { m_DSCreateInfo.depthCompareOp = zCmpOp; }
+    VkCompareOp DSGetZCmpOp() const { m_DSCreateInfo.depthCompareOp; }
+    
+    void DSEnableStencilTest() { m_DSCreateInfo.stencilTestEnable = VK_TRUE; }
+    void DSDisableStenilTest() { m_DSCreateInfo.stencilTestEnable = VK_FALSE; }
+    bool DSGetStencilTestEnabled() const { return m_DSCreateInfo.stencilTestEnable; }
+    void DSSetStencilOp(VkStencilOpState  stencilOp) { m_DSCreateInfo.front = m_DSCreateInfo.back = stencilOp; }
+    VkStencilOpState DSGetStencilOp() const { return m_DSCreateInfo.front; }
+
+    // frame buffer color blending
+    void FBEnableBlend(int attachmentIdx);
+    void FBDisableBlend(int attachmentIdx);
+    bool FBGetBlendEnabled(int attachmentIdx) const;
+    void FBSetColorBlendOp(int attachmentIdx, VkBlendFactor srcFactor, VkBlendFactor dstFactor, VkBlendOp op, VkColorComponentFlags colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT);
+    bool FBGetColorBlendOp(int attachmentIdx, VkBlendFactor& srcFactor, VkBlendFactor& dstFactor, VkBlendOp& op, VkColorComponentFlags& colorWriteMask) const;
+    void FBSetAlphaBlendOp(int attachmentIdx, VkBlendFactor srcFactor, VkBlendFactor dstFactor, VkBlendOp op);
+    bool FBGetAlphaBlendOp(int attachmentIdx, VkBlendFactor& srcFactor, VkBlendFactor& dstFactor, VkBlendOp& op) const;
+
+    // dynamic states
+    void SetDynamicStateHint(VkDynamicState state);
+    void UnsetDynamicStateHint(VkDynamicState state);;
+    bool IsDynamicState(VkDynamicState state) const;
+    int DynamicStateCount() const { return m_DynamicStates.size(); }
+
+    // multi sample state
+    void MSEnableAlphaToCoverage() { m_AlphaToCoverageEnable = true; }
+    void MSDisableAlphaToCoverage() { m_AlphaToCoverageEnable = false; }
+    bool MSAlphaToCoverageEnabled() const { return m_AlphaToCoverageEnable; }
+    void MSSetSampleCount(int sampleCnt);
+    int MSGetSampleCount() const;
+
+    bool Create(VkDevice device, bool forceCreate = false);
+    bool IsCreate() const { return m_Pipeline != VK_NULL_HANDLE; }
+    void Release();
+
+private:
+    void ResetDefaultStates();
+    static VkPipelineColorBlendAttachmentState GetDefaultBlendState()
+    {
+        VkPipelineColorBlendAttachmentState bs;
+        bs.blendEnable = VK_FALSE;
+        bs.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
+        bs.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;;
+        bs.colorBlendOp = VK_BLEND_OP_ADD;;
+        bs.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT;
+        bs.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+        bs.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+        bs.alphaBlendOp = VK_BLEND_OP_ADD;
+        return bs;
+    }
+
+    static VkPipelineRasterizationStateCreateInfo GetDefaultRasterizationState()
+    {
+        VkPipelineRasterizationStateCreateInfo rs{};
+        rs.cullMode = VK_CULL_MODE_BACK_BIT;
+        rs.frontFace = VK_FRONT_FACE_CLOCKWISE;
+        rs.polygonMode = VK_POLYGON_MODE_FILL;
+        rs.lineWidth = 1;;
+        rs.rasterizerDiscardEnable = false;
+        rs.depthBiasEnable = false;
+        rs.depthClampEnable = false;
+        rs.depthBiasConstantFactor = 0;
+        rs.depthBiasSlopeFactor = 0;;
+        rs.depthBiasClamp = 0;
+        return rs;;
+    }
+
+    static VkPipelineDepthStencilStateCreateInfo GetDefaultDepthStencilState()
+    {
+        VkPipelineDepthStencilStateCreateInfo ds{};
+        VkStencilOpState ss{};
+        ss.reference = 0;
+        ss.compareMask = std::numeric_limits<uint32_t>::max();
+        ss.writeMask = std::numeric_limits<uint32_t>::max();
+        ss.compareOp = VK_COMPARE_OP_ALWAYS;
+        ss.passOp = VK_STENCIL_OP_KEEP;
+        ss.failOp = VK_STENCIL_OP_KEEP;
+        ss.depthFailOp = VK_STENCIL_OP_KEEP;
+         
+        ds.depthTestEnable = true;
+        ds.depthBoundsTestEnable = false;
+        ds.depthWriteEnable = true;
+        ds.depthCompareOp = VK_COMPARE_OP_LESS_OR_EQUAL;
+        ds.stencilTestEnable = false;
+        ds.front = ss;
+        ds.back = ss;
+
+        return ds;
+    }
+};
